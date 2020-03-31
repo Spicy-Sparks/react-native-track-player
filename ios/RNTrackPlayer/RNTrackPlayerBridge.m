6d98ffeb82add557f7d264a675f30a635e547063
marf <fontana.marco@hotmail.com>
2020-03-31 17:12:07 +0200

    Update RNTrackPlayerBridge.m

--- ios/RNTrackPlayer/RNTrackPlayerBridge.m
+++ ios/RNTrackPlayer/RNTrackPlayerBridge.m
@@ -12,17 +12,23 @@
 
 @interface RCT_EXTERN_REMAP_MODULE(TrackPlayerModule, RNTrackPlayer, NSObject)
 
-RCT_EXTERN_METHOD(setupPlayer);
+RCT_EXTERN_METHOD(updateOptions:(NSDictionary *)options:
+                  (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject);
 
-RCT_EXTERN_METHOD(updateOptions:(NSDictionary *)options);
+RCT_EXTERN_METHOD(setNowPlaying:(NSDictionary *)object:
+                  (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject);
 
-RCT_EXTERN_METHOD(setNowPlaying:(NSDictionary *)object);
-
-RCT_EXTERN_METHOD(updatePlayback:(NSDictionary *)properties);
+RCT_EXTERN_METHOD(updatePlayback:(NSDictionary *)properties:
+                  (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject);
 
 RCT_EXTERN_METHOD(reset:(RCTPromiseResolveBlock)resolve
-rejecter:(RCTPromiseRejectBlock)reject);
+                  rejecter:(RCTPromiseRejectBlock)reject);
 
-RCT_EXTERN_METHOD(updateMetadataForTrack:properties:(NSDictionary *)properties);
+RCT_EXTERN_METHOD(updateMetadataForTrack:properties:(NSDictionary *)properties:
+                  (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject);
 
 @end
