b70c318d9e85fd708a11e2a05a052e55644a46f2
Marco Fontana <fontana.marco@hotmail.com>
2021-05-02 11:31:59 +0200

    Update MusicService.java

--- android/src/main/java/com/guichaguri/trackplayer/service/MusicService.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/MusicService.java
@@ -69,19 +69,15 @@ public class MusicService extends HeadlessJsTaskService {
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 
-    public void destroy() {
+    public void destroy(boolean intentToStop) {
         if(handler != null) {
             handler.removeMessages(0);
             handler = null;
         }
 
         if(manager != null) {
-            if (!intentToStop) {
-                cachePlayer();
-            } else {
-                manager.destroy(intentToStop);
-                manager = null;
-            }
+            manager.destroy(intentToStop);
+            manager = null;
         }
     }
 
@@ -164,14 +160,6 @@ public class MusicService extends HeadlessJsTaskService {
             if (manager != null && manager.getMetadata().getSession() != null) {
                 MediaButtonReceiver.handleIntent(manager.getMetadata().getSession(), intent);
                 return START_NOT_STICKY;
-            } else if (manager == null) {
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                String cachedCurrentTrack = prefs.getString("cachedCurrentTrack", null);
-                if (cachedCurrentTrack != null) {
-                    manager = new MusicManager(this);
-                    recoverLostPlayer(intentExtra.getKeyCode());
-                    return START_REDELIVER_INTENT;
-                }
             }
         }
 
@@ -183,92 +171,6 @@ public class MusicService extends HeadlessJsTaskService {
 
         super.onStartCommand(intent, flags, startId);
         return START_NOT_STICKY;
-    }
-
-    private void recoverLostPlayer(Integer keycode) {
-
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-
-        // Return if stop (swipe away)
-        if (keycode == KEYCODE_MEDIA_STOP) {
-            clearCache();
-            return;
-        }
-
-        // Get current track and return if it is not available
-        String cachedCurrentTrack = prefs.getString("cachedCurrentTrack", null);
-        if (cachedCurrentTrack == null) return;
-
-        // Get back player options
-        String cachedOptionsJsonString = prefs.getString("cachedOptions", null);
-        Bundle optionsBundle = jsonStringToBundle(cachedOptionsJsonString);
-
-        // Get current track position
-        Integer ratingType = optionsBundle.getInt("ratingType", RatingCompat.RATING_NONE);
-        Track currentTrack = new Track(getApplicationContext(), jsonStringToBundle(cachedCurrentTrack), ratingType);
-        Long currentPosition = prefs.getLong("cachedPosition", 0);
-        Integer resumeAt = 0;
-
-        // Set player options
-        manager.getMetadata().updateOptions(optionsBundle);
-        manager.getMetadata().updateMetadata(currentTrack);
-        //playback.seekTo(currentPosition);
-
-        // Act on event
-        ButtonEvents buttonEvents = new ButtonEvents(this, manager);
-        if (keycode == KEYCODE_MEDIA_PLAY) {
-            //playback.play();
-        } else if (keycode == KEYCODE_MEDIA_NEXT) {
-            buttonEvents.onSkipToNext();
-        } else if (keycode == KEYCODE_MEDIA_FAST_FORWARD) {
-            buttonEvents.onFastForward();
-        } else if (keycode == KEYCODE_MEDIA_REWIND) {
-            buttonEvents.onRewind();
-        } else if (keycode == KEYCODE_MEDIA_PREVIOUS) {
-            buttonEvents.onSkipToPrevious();
-        } else {
-            Log.d(Utils.LOG, "keyCode " + keycode + " is not handled");
-        }
-
-        // Clear cache
-        clearCache();
-    }
-
-    private void clearCache() {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.remove("cachedCurrentTrack");
-        editor.remove("cachedPosition");
-        editor.remove("cachedQueue");
-        editor.remove("cachedOptions");
-        editor.apply();
-    }
-
-    private void cachePlayer() {
-        if(manager == null)
-            return;
-        // Make editor
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor editor = prefs.edit();
-
-        // Cache current track
-        Track currentTrack = manager.getCurrentTrack();
-        if (currentTrack == null) return;
-        editor.putString("cachedCurrentTrack", currentTrack.json.toString());
-
-        // Cache current track position
-        //Long currentPosition = playback.getPosition();
-        //editor.putLong("cachedPosition", currentPosition);
-
-        // Cache options
-        Bundle options = manager.getMetadata().getOptionsBundle();
-        editor.putString("cachedOptions", bundleToJson(options).toString());
-
-        editor.apply();
-
-        manager.destroy(intentToStop);
-        manager = null;
-
     }
 
     public void startServiceOreoAndAbove(){
@@ -291,13 +193,9 @@ public class MusicService extends HeadlessJsTaskService {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (!intentToStop) {
-            cachePlayer();
-        } else {
-            if(manager != null) {
-                manager.destroy(intentToStop);
-                manager = null;
-            }
+        if(manager != null) {
+            manager.destroy(true);
+            manager = null;
         }
 
         stopForeground(true);
@@ -309,7 +207,7 @@ public class MusicService extends HeadlessJsTaskService {
         super.onTaskRemoved(rootIntent);
 
         if (manager == null || manager.shouldStopWithApp()) {
-            destroy();
+            destroy(true);
             stopSelf();
         }
     }
